618d1ea870491ab1a313dfddf0af0c26bb1d494b
zoro-nintoryu <markocop40@gmail.com>
2023-08-17 15:52:18 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+const header = document.querySelector("header");
+window.addEventListener("scroll", function () {
+  header.classList.toggle("sticky", window.scrollY > 100);
+});
+
+let menu = document.querySelector("#menu-icon");
+let navlist = document.querySelector(".navlist");
+
+menu.onclick = () => {
+  menu.classList.toggle("bx-x");
+  navlist.classList.toggle("open");
+};
+
+window.onscroll = () => {
+  menu.classList.remove("bx-x");
+  navlist.classList.remove("open");
+};
+
+var btn = document.querySelector('.btn')
+
+btn.onclick = () => {
+  alert('waalaikumsalam');
+}
+
+var submit = document.querySelector('.submit')
+
+submit.onclick = () => {
+  alert('maaf server belum selesai🙏');
+}
